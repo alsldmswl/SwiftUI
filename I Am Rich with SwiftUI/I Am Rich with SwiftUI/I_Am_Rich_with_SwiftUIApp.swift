10ea3428f6c1b5d7d8f207136fff7b2768bc82f5
alsldmswl <jej1452@gmail.com>
2023-04-19 17:24:07 +0900

    Add new project: I Am Rich

---
+++ I Am Rich with SwiftUI/I Am Rich with SwiftUI/I_Am_Rich_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_with_SwiftUIApp.swift
+//  I Am Rich with SwiftUI
+//
+//  Created by eun-ji on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
