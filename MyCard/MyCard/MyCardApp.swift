7b801a5eddcb2ddbdb04f8a9e0d37e91415a0fcb
alsldmswl <jej1452@gmail.com>
2023-04-21 22:34:52 +0900

    Add new project: MyCard

---
+++ MyCard/MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by eun-ji on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
