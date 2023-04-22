4a6f560922a8bea73dc3b8751f58fc999343b51e
alsldmswl <jej1452@gmail.com>
2023-04-22 22:04:18 +0900

    Add new project: Dicer

---
+++ Dicer/Dicer/DicerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicerApp.swift
+//  Dicer
+//
+//  Created by eun-ji on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct DicerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
